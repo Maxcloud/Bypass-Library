6f2f9db4daf611e0020fcd8427f179cf1c2254e8
Maxcloud <Maxcloud@users.noreply.github.com>
2024-06-28 18:20:05 -0400

    Fix spacing

--- log/log.cpp
+++ log/log.cpp
@@ -3,36 +3,36 @@
 namespace Log
 {
 
-	void Log(const char* format, ...)
-	{
-		char buf[1024] = { 0 };
+    void Log(const char* format, ...)
+    {
+        char buf[1024] = { 0 };
 
-		va_list args;
-		va_start(args, format);
-		vsprintf(buf, format, args);
+        va_list args;
+        va_start(args, format);
+        vsprintf(buf, format, args);
 
-		OutputDebugStringA(buf);
+        OutputDebugStringA(buf);
 
-		va_end(args);
-	}
+        va_end(args);
+    }
 
-	void LogHexDump(unsigned char* ptr, int len)
-	{
-		char buf[8192] = { 0 };
-		char tmp[16] = { 0 };
+    void LogHexDump(unsigned char* ptr, int len)
+    {
+        char buf[8192] = { 0 };
+        char tmp[16] = { 0 };
 
-		int bufPos = 0;
+        int bufPos = 0;
 
-		for (int i = 0; i < len; i++)
-		{
-			auto value = ptr[i];
+        for (int i = 0; i < len; i++)
+        {
+            auto value = ptr[i];
 
-			memset(tmp, 0, 16);
-			sprintf(tmp, "%02X ", value);
-			strcat(buf, tmp);
-		}
+            memset(tmp, 0, 16);
+            sprintf(tmp, "%02X ", value);
+            strcat(buf, tmp);
+        }
 
-		Log("[HexDump] %s", buf);
-	}
+        Log("[HexDump] %s", buf);
+    }
 
 }
